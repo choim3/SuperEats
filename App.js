92e917374bb30c1e17ea7d27d17f239f9069e06a
matt <choim3@spu.edu>
2021-12-24 12:45:40 -0800

    app

--- App.js
+++ App.js
@@ -4,5 +4,10 @@ import Home from "./screens/Home";
 import RestaurantDetails from "./screens/RestaurantDetails";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      {" "}
+      <Home />{" "}
+    </>
+  );
 }
